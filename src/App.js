d94efe1db5693210fcb8892634745f88e502f3e5
Parvathi <pava@uw.edu>
2020-09-07 12:40:25 -0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import Donate from './components/pages/home';
+import Organization from './components/pages/staff';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="container">
+      <Route path="/" exact={true} component={Donate}/>
+      <Route path="/staff" exact={true} component={Organization}/>
     </div>
+    </div>
+    </Router>
   );
 }
 
